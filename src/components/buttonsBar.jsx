102b607b9160becaee5050c2d07f2781767dff01
18mpr1 <matthew.paul.rieckenberg@gmail.com>
2021-07-31 03:27:47 -0400

    Changes to button lighting.

--- src/components/buttonsBar.jsx
+++ src/components/buttonsBar.jsx
@@ -12,11 +12,11 @@ function ButtonsBar(){
             <h1 id="title"><b>Algo-Explorer</b></h1>
             <div className="buttons">
 
-                <button className="pulse">Cocktail Sort</button>
-                <button className="close">Bogo Sort</button>
-                <button className="raise">Merge Sort</button>
-                <button className="up">Quick Sort</button>
-                <button className="slide">Bubble Sort</button>
+                <button className="fill">Cocktail Sort</button>
+                <button className="fill">Bogo Sort</button>
+                <button className="fill">Merge Sort</button>
+                <button className="fill">Quick Sort</button>
+                <button className="fill">Bubble Sort</button>
                 <button className="offset">Reset</button>
 
                 {/*<button className="fill">Fill</button>
