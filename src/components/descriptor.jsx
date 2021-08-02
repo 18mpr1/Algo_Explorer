057a5114502e8932676dc2a8d94765b40db46e5b
18mpr1 <matthew.paul.rieckenberg@gmail.com>
2021-08-02 12:22:05 -0400

    Unsorted array bars showing from sandbox.jsx

---
+++ src/components/descriptor.jsx
@@ -0,0 +1,13 @@
+// This is where the algorithm's information is display like a dropdown menu
+
+function Descriptor(){
+    return(
+        <div id="description">
+            <h1>Algorithm Title</h1>
+            <p>Description will go here</p>
+        </div>
+
+    )
+}
+
+export default Descriptor;
