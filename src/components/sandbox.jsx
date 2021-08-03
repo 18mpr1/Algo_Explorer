6170fdc4237b51b601e256cee4ce56f20e73d5e3
18mpr1 <matthew.paul.rieckenberg@gmail.com>
2021-08-03 13:10:02 -0400

    Minor colour changes to the UI

--- src/components/sandbox.jsx
+++ src/components/sandbox.jsx
@@ -8,7 +8,7 @@ const ANIMATION_SPEED_MS = 1;
 const NUMBER_OF_ARRAY_BARS = 290;
 
 // This is the main color of the array bars.
-const PRIMARY_COLOR = 'turquoise';
+const PRIMARY_COLOR = 'purple';
 
 // This is the color of array bars that are being compared throughout the animations.
 const SECONDARY_COLOR = 'red';
